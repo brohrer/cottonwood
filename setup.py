c3658fc5191f3df6c753fc65a68b5115d33fa676
brohrer <brohrer@gmail.com>
2019-11-02 17:37:17 -0400

    files removed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 st.setup(
     name='cottonwood',
-    version='6.2',
+    version='7',
     description='A flexible machine learning framework',
     url='http://github.com/brohrer/cottonwood',
     download_url='https://github.com/brohrer/cottonwood/tags/',
