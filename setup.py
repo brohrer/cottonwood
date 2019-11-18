85f1fc10ff81e6120d168a175d01fd91c889c5a0
brohrer <brohrer@gmail.com>
2019-11-17 19:59:16 -0500

    prep for version 9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 st.setup(
     name='cottonwood',
-    version='9',
+    version='8.1',
     description='A flexible machine learning framework',
     url='http://github.com/brohrer/cottonwood',
     download_url='https://github.com/brohrer/cottonwood/tags/',
